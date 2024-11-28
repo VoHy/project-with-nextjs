30b38eb21a3caa9b10e113a6f088b636e4407414
Guin <vohyyy321@gmail.com>
2024-11-28 19:03:11 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>Hello Word
+      <div>
+        <Button type="primary" >Add new</Button>
+      </div>
+    </div>
+  );
+}
